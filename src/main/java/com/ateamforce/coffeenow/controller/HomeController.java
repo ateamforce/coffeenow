5f0fb25bce3b96e22e423a783130ac2272bccb69
Panagiotis Sakellariou <sakellariou23@hotmail.com>
2019-07-10 00:29:18 +0300

    some mappings added

--- src/main/java/com/ateamforce/coffeenow/controller/HomeController.java
+++ src/main/java/com/ateamforce/coffeenow/controller/HomeController.java
@@ -8,27 +8,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
-	// RedirectAttributes are needed in order to pass attributes to the redirect
-	// view. not needed when forward is used instead
-	@RequestMapping
-	public String home(Model model) {
-		model.addAttribute("greeting", "Welcome to Web Store!");
-		model.addAttribute("tagline", "The one and only amazing web store");
-		return "front/login";
-	}
+    // No spring security locking needed
+    @RequestMapping
+    public String home(Model model) {
+        return "front/login";
+    }
 
-//  SAME AS ABOVE (not good cause of tight coupling with InternalResourceView)
-//	@RequestMapping
-//	public ModelAndView welcome(Map<String, Object> model) {
-//		model.put("greeting", "Welcome to Web Store!");
-//		model.put("tagline", "The one and only amazing web store");
-//		View view = new InternalResourceView("/WEB-INF/views/welcome.jsp");
-//		return new ModelAndView(view, model);
-//	}
+    // No spring security locking needed
+    @RequestMapping("/admin")
+    public String admin_login() {
+            return "back_admin/login";
+    }
 
-	@RequestMapping("/admin")
-	public String admin_login() {
-		return "back_admin/login";
-	}
+    // secure this
+    @RequestMapping("/admin/dashboard")
+    public String admin_dashboard() {
+            return "back_admin/dashboard/index";
+    }
+
+    // No spring security locking needed
+    @RequestMapping("/store")
+    public String store_login() {
+            return "back_store/login";
+    }
+
+    // secure this
+    @RequestMapping("/store/dashboard")
+    public String store_dashboard() {
+            return "back_store/dashboard/index";
+    }
 
 }
