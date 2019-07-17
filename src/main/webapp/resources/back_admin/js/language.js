ef867c3c7325873e5d063d941f80953a3920fba1
Panagiotis Sakellariou <sakellariou23@hotmail.com>
2019-07-17 05:11:32 +0300

    admin dashboard 1.1.20
    - added delete functionality to datatables with verify message
    - added front end internationalization

---
+++ src/main/webapp/resources/back_admin/js/language.js
@@ -0,0 +1,13 @@
+var language_JSON = {
+	"el":{
+		"yes":"Ναί",
+		"no":"Όχι",
+		"areYouSure":"Είσαι σίγουρος/η;"
+	},
+	"en":{
+		"yes":"Yes",
+		"no":"No",
+		"areYouSure":"Are you sure\?"
+	}
+};
+var locale = document.getElementById("languagesCFN").getAttribute("data-cfn-locale");
