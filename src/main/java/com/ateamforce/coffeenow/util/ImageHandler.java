d0480dbb51f06e2bb10ab9566a32838fadf1d85b
alexandrosporfiris <alexandrosporfiris@hotmail.gr>
2019-07-31 08:30:34 +0300

    resize

--- src/main/java/com/ateamforce/coffeenow/util/ImageHandler.java
+++ src/main/java/com/ateamforce/coffeenow/util/ImageHandler.java
@@ -1,7 +1,10 @@
 package com.ateamforce.coffeenow.util;
 
 import com.ateamforce.coffeenow.model._ImageCarrier;
+import java.awt.AlphaComposite;
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -20,6 +23,9 @@ public class ImageHandler implements ImageHandlerService {
 
     private final static Logger LOGGER = Logger.getLogger(ImageHandler.class);
 
+    private static final int IMG_WIDTH = 100;
+    private static final int IMG_HEIGHT = 100;
+
     /**
      * Saves an image to path with id as filename
      *
@@ -37,6 +43,7 @@ public class ImageHandler implements ImageHandlerService {
         File localFile = new File("/");
         String realPath = this.getClass().getClassLoader().getResource("").getPath();
         String fullPath = "";
+
         try {
             fullPath = URLDecoder.decode(realPath, "UTF-8");
 
@@ -46,16 +53,20 @@ public class ImageHandler implements ImageHandlerService {
             // TODO: process image. compress, resize etc
             try {
 
-                BufferedImage img = ImageIO.read(businessObj.getImage().getInputStream());
+                BufferedImage originalImage = ImageIO.read(businessObj.getImage().getInputStream());
+                int type = originalImage.getType() == 0? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
+                
                 // TODO: only keep one of the two on live environment. or put a new path entirely.
                 // Copy image in both destinations, because tomcat has a delay in copying from the first to the 2nd automatically,
                 // so when the page loads, the image that is supposed to load is at the 2nd path, but tomcat, hasn't put it there yet
-                BufferedImage newImage = new BufferedImage(img.getWidth(),
-                        img.getHeight(), BufferedImage.TYPE_INT_RGB);
-                newImage.createGraphics().drawImage(img, 0, 0, Color.WHITE, null);
+
+                BufferedImage resizeImageJpg = resizeImage(originalImage, type);
+                
                 localFile = new File(pathArr[0] + "/src/main/webapp" + path + id.toString() + ".jpg");
-                ImageIO.write(newImage, "jpg", localFile);
-                ImageIO.write(newImage, "jpg", new File(pathArr[0] + "/target/coffeenow-1.0" + path + id.toString() + ".jpg"));
+                ImageIO.write(resizeImageJpg,
+                        "jpg", localFile);
+                ImageIO.write(resizeImageJpg,
+                        "jpg", new File(pathArr[0] + "/target/coffeenow-1.0" + path + id.toString() + ".jpg"));
             } catch (IOException | IllegalStateException e) {
                 throw new RuntimeException("Product Image saving failed", e);
             }
@@ -67,6 +78,24 @@ public class ImageHandler implements ImageHandlerService {
 
         return (!localFile.equals(new File("/")) && localFile.exists());
     }
+
+    private static BufferedImage resizeImage(BufferedImage originalImage, int type){
+		
+	BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
+	Graphics2D g = resizedImage.createGraphics();
+	g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
+	g.dispose();	
+	g.setComposite(AlphaComposite.Src);
+
+	g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+	RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+	g.setRenderingHint(RenderingHints.KEY_RENDERING,
+	RenderingHints.VALUE_RENDER_QUALITY);
+	g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+	RenderingHints.VALUE_ANTIALIAS_ON);
+	
+	return resizedImage;
+    }	
 
     @Override
     public void deleteImage(String path, Integer id) {
