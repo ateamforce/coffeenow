d7ee70a640a38bd20672b181417225e7d65c9179
Panagiotis Sakellariou <sakellariou23@hotmail.com>
2019-08-12 10:02:56 +0300

    reset password 1.0.11

--- src/main/java/com/ateamforce/coffeenow/util/PasswordService.java
+++ src/main/java/com/ateamforce/coffeenow/util/PasswordService.java
@@ -2,7 +2,8 @@ package com.ateamforce.coffeenow.util;
 
 /**
  * 
- * An interface mean to be implemented by business objects that have a password associated with them
+ * An interface meant to be implemented by business objects that have a password associated with them.
+ * Works in combination with {@link com.ateamforce.coffeenow.validator.PasswordMatchesValidator @PasswordMatches} annotation
  *
  * @author Sakel
  */
